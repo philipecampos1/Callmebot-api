b7fe05c893dba56bc3fed61277229952766ed15c
philipe campos <142898955+philipecampos1@users.noreply.github.com>
2024-09-24 20:44:20 +0100

    Update callmebot_service.py

--- clients/callmebot_service.py
+++ clients/callmebot_service.py
@@ -7,9 +7,10 @@ class Callmebot:
     def __init__(self):
         self.__base_url = 'https://api.callmebot.com/whatsapp.php'
         self.__api_key = hide.api_whatsapp
+        self.__my_number = hide.my_number
 
     def send_message(self, message):
         response = requests.get(
-            url=f'{self.__base_url}?phone=447432661686&text={message}&apikey={self.__api_key}'
+            url=f'{self.__base_url}?phone={self.__my_number}&text={message}&apikey={self.__api_key}'
         )
         return response.text
